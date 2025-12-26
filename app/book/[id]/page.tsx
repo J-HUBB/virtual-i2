c907a45a5a661990c26269012f8c665e583f03a6
J-HUBB <lowlifeenterprizes@yahoo.com>
2025-12-26 03:49:38 -0500

    Still trying to Implement add to library

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -35,9 +35,27 @@ const BooksPage = () => {
   console.log(useGetBookByIdQuery(id));
 
   const handleAdd = async () => {
-    if (!isAuthenticated || !user?.uid || !book?.id)
-      return alert("Please log in first!");
-
+    if (!isAuthenticated || !user) {
+      dispatch(openModal());
+    }
+    else if (isAuthenticated) {
+      <div className="inner-book__bookmark">
+        <div className="inner-book__bookmark--icon">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"></path>
+          </svg>
+        </div>
+        <div className="inner-book__bookmark--text">Saved in My Library</div>
+      </div>;
+    
     const bookRef = doc(db, "users", user.uid, "library", book.id);
     await setDoc(
       bookRef,
@@ -50,6 +68,7 @@ const BooksPage = () => {
       },
       { merge: true }
     ); //Use merge to avoid overwriting a "finished" status
+  }
   };
 
   const handleRemove = async () => {
@@ -320,7 +339,7 @@ const BooksPage = () => {
                     )}
                   </div>
 
-                  <div className="inner-book__bookmark">
+                  <div className="inner-book__bookmark" onClick={handleAdd}>
                     <div className="inner-book__bookmark--icon">
                       <svg
                         stroke="currentColor"
@@ -334,33 +353,7 @@ const BooksPage = () => {
                         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"></path>
                       </svg>
                     </div>
-                    <div
-                      className="inner-book__bookmark--text"
-                      onClick={() => {
-                        if (!isAuthenticated) {
-                          dispatch(openModal());
-                        } else if (isAuthenticated) {
-                          <>
-                            <div className="inner-book__bookmark--icon">
-                              <svg
-                                stroke="currentColor"
-                                fill="currentColor"
-                                strokeWidth="0"
-                                viewBox="0 0 16 16"
-                                height="1em"
-                                width="1em"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"></path>
-                              </svg>
-                            </div>
-                            <div className="inner-book__bookmark--text">
-                              Saved in My Library
-                            </div>
-                          </>;
-                        }
-                      }}
-                    >
+                    <div className="inner-book__bookmark--text">
                       Add title to My Library
                     </div>
                   </div>
