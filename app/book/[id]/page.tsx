18bf6d05c4e8037b32b4dfcefc086cc567dcb6ff
J-HUBB <lowlifeenterprizes@yahoo.com>
2026-01-06 14:18:58 -0500

    Fixed button bug, but no payments still

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -86,7 +86,6 @@ const BooksPage = () => {
     }
   };
 
-
   const audioRef = useRef<HTMLAudioElement>(null);
   const [duration, setDuration] = useState<number>(0);
 
@@ -302,27 +301,38 @@ const BooksPage = () => {
                       <div className="inner-book__read--text">Read</div>
                     </button>
 
-                    {isAuthenticated && isSubscribed ? (
-                      <button
-                        className="inner-book__read--btn"
-                        onClick={() => `/player/${id}`}
-                      >
-                        <div className="inner-book__read--icon">
-                          <svg
-                            stroke="currentColor"
-                            fill="currentColor"
-                            strokeWidth="0"
-                            viewBox="0 0 1024 1024"
-                            height="1em"
-                            width="1em"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M842 454c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8 0 140.3-113.7 254-254 254S258 594.3 258 454c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8 0 168.7 126.6 307.9 290 327.6V884H326.7c-13.7 0-24.7 14.3-24.7 32v36c0 4.4 2.8 8 6.2 8h407.6c3.4 0 6.2-3.6 6.2-8v-36c0-17.7-11-32-24.7-32H548V782.1c165.3-18 294-158 294-328.1zM512 624c93.9 0 170-75.2 170-168V232c0-92.8-76.1-168-170-168s-170 75.2-170 168v224c0 92.8 76.1 168 170 168zm-94-392c0-50.6 41.9-92 94-92s94 41.4 94 92v224c0 50.6-41.9 92-94 92s-94-41.4-94-92V232z"></path>
-                          </svg>
-                        </div>
-                        <div className="inner-book__read--text">Listen</div>
-                      </button>
-                    ) : (
+                    {/* {isAuthenticated && isSubscribed ? ( */}
+                    <button
+                      className="inner-book__read--btn"
+                      onClick={() => {
+                        if (!isAuthenticated) {
+                          dispatch(openModal());
+                        } else if (
+                          !isSubscribed &&
+                          book?.subscriptionRequired
+                        ) {
+                          router.push("/choose-plan");
+                        } else {
+                          router.push(`/player/${id}`);
+                        }
+                      }}
+                    >
+                      <div className="inner-book__read--icon">
+                        <svg
+                          stroke="currentColor"
+                          fill="currentColor"
+                          strokeWidth="0"
+                          viewBox="0 0 1024 1024"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M842 454c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8 0 140.3-113.7 254-254 254S258 594.3 258 454c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8 0 168.7 126.6 307.9 290 327.6V884H326.7c-13.7 0-24.7 14.3-24.7 32v36c0 4.4 2.8 8 6.2 8h407.6c3.4 0 6.2-3.6 6.2-8v-36c0-17.7-11-32-24.7-32H548V782.1c165.3-18 294-158 294-328.1zM512 624c93.9 0 170-75.2 170-168V232c0-92.8-76.1-168-170-168s-170 75.2-170 168v224c0 92.8 76.1 168 170 168zm-94-392c0-50.6 41.9-92 94-92s94 41.4 94 92v224c0 50.6-41.9 92-94 92s-94-41.4-94-92V232z"></path>
+                        </svg>
+                      </div>
+                      <div className="inner-book__read--text">Listen</div>
+                    </button>
+                    {/* ) : (
                       <button
                         className="inner-book__read--btn"
                         onClick={() => dispatch(openModal())}
@@ -342,7 +352,7 @@ const BooksPage = () => {
                         </div>
                         <div className="inner-book__read--text">Listen</div>
                       </button>
-                    )}
+                    )} */}
                   </div>
 
                   <div className="inner-book__bookmark" onClick={handleAdd}>
